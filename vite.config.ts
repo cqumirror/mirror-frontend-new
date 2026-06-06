9572881bf3015323777f79be4850e5ba232a332f
Tano <github@tano.asia>
2026-06-06 15:55:42 +0800

    feat: 增加测试域名许可

--- vite.config.ts
+++ vite.config.ts
@@ -37,7 +37,7 @@ export default defineConfig({
     alias: { '@': resolve(__dirname, 'src') },
   },
 
-  server: { port: 3000, proxy: proxyConfig },
+  server: { port: 3000, allowedHosts: ['mirrors.tano.asia'], proxy: proxyConfig },
   preview: { port: 4173, proxy: proxyConfig },
 
   build: {
